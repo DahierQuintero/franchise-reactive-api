53dd2ee4d775921c6b2c7b421f2256dd6a2cba73
Quinterito <dilandahier@gmail.com>
2026-01-27 12:09:40 -0500

    Initial project setup with Clean Architecture structure

---
+++ src/main/java/com/accenture/franchise/franchise_reactive_api/FranchiseReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.franchise.franchise_reactive_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/franchise/franchise_reactive_api/FranchiseReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.franchise.franchise_reactive_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
