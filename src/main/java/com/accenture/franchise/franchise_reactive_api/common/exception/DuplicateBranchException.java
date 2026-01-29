429e4f404b3a19cc0df37a1aa430203b030528db
Quinterito <dilandahier@gmail.com>
2026-01-29 00:08:30 -0500

    Add common exceptions and shared utilities

---
+++ src/main/java/com/accenture/franchise/franchise_reactive_api/common/exception/DuplicateBranchException.java
@@ -0,0 +1,8 @@
+package com.accenture.franchise.franchise_reactive_api.common.exception;
+
+public class DuplicateBranchException extends RuntimeException {
+
+    public DuplicateBranchException(String message) {
+        super(message);
+    }
+}
