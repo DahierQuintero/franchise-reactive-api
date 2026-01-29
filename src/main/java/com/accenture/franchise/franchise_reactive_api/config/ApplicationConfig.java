54fb57052e02e7a93b12b08bc3b0935d9f4dee14
Quinterito <dilandahier@gmail.com>
2026-01-29 00:10:16 -0500

    Configure application beans for reactive services

--- src/main/java/com/accenture/franchise/franchise_reactive_api/config/ApplicationConfig.java
+++ src/main/java/com/accenture/franchise/franchise_reactive_api/config/ApplicationConfig.java
@@ -1,7 +1,11 @@
 package com.accenture.franchise.franchise_reactive_api.config;
 
 import com.accenture.franchise.franchise_reactive_api.application.service.AddBranchToFranchiseService;
+import com.accenture.franchise.franchise_reactive_api.application.service.AddProductToBranchService;
 import com.accenture.franchise.franchise_reactive_api.application.service.CreateFranchiseService;
+import com.accenture.franchise.franchise_reactive_api.application.service.DeleteProductFromBranchService;
+import com.accenture.franchise.franchise_reactive_api.application.service.GetTopStockProductsByFranchiseService;
+import com.accenture.franchise.franchise_reactive_api.application.service.UpdateProductStockService;
 import com.accenture.franchise.franchise_reactive_api.domain.repository.IFranchiseRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,5 +25,33 @@ public class ApplicationConfig {
             IFranchiseRepository franchiseRepository
     ) {
         return new AddBranchToFranchiseService(franchiseRepository);
+    }
+
+    @Bean
+    public AddProductToBranchService addProductToBranchService (
+            IFranchiseRepository franchiseRepository
+    ) {
+        return new AddProductToBranchService(franchiseRepository);
+    }
+
+    @Bean
+    public UpdateProductStockService updateProductStockService (
+            IFranchiseRepository franchiseRepository
+    ) {
+        return new UpdateProductStockService(franchiseRepository);
+    }
+
+    @Bean
+    public DeleteProductFromBranchService deleteProductFromBranchService (
+            IFranchiseRepository franchiseRepository
+    ) {
+        return new DeleteProductFromBranchService(franchiseRepository);
+    }
+
+    @Bean
+    public GetTopStockProductsByFranchiseService getTopStockProductService (
+            IFranchiseRepository franchiseRepository
+    ) {
+        return new GetTopStockProductsByFranchiseService(franchiseRepository);
     }
 }
