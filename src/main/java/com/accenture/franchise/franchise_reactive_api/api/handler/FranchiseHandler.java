7048545c68107271290978bb6b734b50cd11f112
Quinterito <dilandahier@gmail.com>
2026-01-29 00:09:34 -0500

    Expose reactive endpoints for franchise, branch and product operations

--- src/main/java/com/accenture/franchise/franchise_reactive_api/api/handler/FranchiseHandler.java
+++ src/main/java/com/accenture/franchise/franchise_reactive_api/api/handler/FranchiseHandler.java
@@ -2,17 +2,22 @@ package com.accenture.franchise.franchise_reactive_api.api.handler;
 
 import com.accenture.franchise.franchise_reactive_api.api.dto.CreateBranchRequest;
 import com.accenture.franchise.franchise_reactive_api.api.dto.CreateFranchiseRequest;
+import com.accenture.franchise.franchise_reactive_api.api.dto.CreateProductRequest;
 import com.accenture.franchise.franchise_reactive_api.api.dto.FranchiseResponse;
+import com.accenture.franchise.franchise_reactive_api.api.dto.UpdateStockRequest;
 import com.accenture.franchise.franchise_reactive_api.application.service.AddBranchToFranchiseService;
+import com.accenture.franchise.franchise_reactive_api.application.service.AddProductToBranchService;
 import com.accenture.franchise.franchise_reactive_api.application.service.CreateFranchiseService;
+import com.accenture.franchise.franchise_reactive_api.application.service.DeleteProductFromBranchService;
+import com.accenture.franchise.franchise_reactive_api.application.service.GetTopStockProductsByFranchiseService;
+import com.accenture.franchise.franchise_reactive_api.application.service.UpdateProductStockService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +25,10 @@ public class FranchiseHandler {
 
     private final CreateFranchiseService createFranchiseService;
     private final AddBranchToFranchiseService addBranchToFranchiseService;
+    private final AddProductToBranchService addProductToBranchService;
+    private final UpdateProductStockService updateProductStockService;
+    private final DeleteProductFromBranchService deleteProductFromBranchService;
+    private final GetTopStockProductsByFranchiseService getTopStockProductsByFranchiseService;
 
     public Mono<ServerResponse> createFranchise(ServerRequest request) {
         return request.bodyToMono(CreateFranchiseRequest.class)
@@ -37,10 +46,69 @@ public class FranchiseHandler {
     public Mono<ServerResponse> addBranch(ServerRequest request) {
         String franchiseId = request.pathVariable("franchiseId");
 
+
         return request.bodyToMono(CreateBranchRequest.class)
                 .flatMap(req ->
                         addBranchToFranchiseService.addBranch(franchiseId, req.name())
                 )
+                .then(ServerResponse.status(HttpStatus.CREATED).build());
+    }
+
+    public Mono<ServerResponse> addProduct(ServerRequest request) {
+        String franchiseId = request.pathVariable("franchiseId");
+        String branchName = request.pathVariable("branchName");
+
+        return request.bodyToMono(CreateProductRequest.class)
+                .flatMap(req ->
+                        addProductToBranchService.addProduct(
+                                franchiseId,
+                                branchName,
+                                req.name(),
+                                req.stock()
+                        )
+                )
+                .then(ServerResponse.status(HttpStatus.CREATED).build());
+    }
+
+    public Mono<ServerResponse> updateProductStock(ServerRequest request) {
+        String franchiseId = request.pathVariable("franchiseId");
+        String branchName = request.pathVariable("branchName");
+        String productName = request.pathVariable("productName");
+
+
+        return request.bodyToMono(UpdateStockRequest.class)
+                .flatMap(req ->
+                        updateProductStockService.updateStock(
+                                franchiseId,
+                                branchName,
+                                productName,
+                                req.stock()
+                        )
+                )
+                .then(ServerResponse.ok().build());
+    }
+
+    public Mono<ServerResponse> deleteProduct(ServerRequest request) {
+        String franchiseId = request.pathVariable("franchiseId");
+        String branchName = request.pathVariable("branchName");
+        String productName = request.pathVariable("productName");
+
+
+        return deleteProductFromBranchService
+                .delete(franchiseId, branchName, productName)
                 .then(ServerResponse.noContent().build());
+    }
+
+    public Mono<ServerResponse> getTopStockProductsByFranchise(ServerRequest request) {
+        String franchiseId = request.pathVariable("franchiseId");
+
+
+        return getTopStockProductsByFranchiseService
+                .getTopStockByFranchise(franchiseId)
+                .flatMap(list ->
+                        list.isEmpty()
+                                ? ServerResponse.notFound().build()
+                                : ServerResponse.ok().bodyValue(list)
+                );
     }
 }
--- src/main/java/com/accenture/franchise/franchise_reactive_api/api/router/FranchiseRouter.java
+++ src/main/java/com/accenture/franchise/franchise_reactive_api/api/router/FranchiseRouter.java
@@ -4,19 +4,22 @@ import com.accenture.franchise.franchise_reactive_api.api.handler.FranchiseHandl
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class FranchiseRouter {
 
     @Bean
     public RouterFunction<ServerResponse> franchiseRoutes(FranchiseHandler handler) {
-        return route(POST("/franchises"), handler::createFranchise)
-                .andRoute(POST("/franchises/{franchiseId}/branches"), handler::addBranch);
+        return RouterFunctions
+                .route()
+                .POST("/franchises", handler::createFranchise)
+                .POST("/franchises/{franchiseId}/branches", handler::addBranch)
+                .POST("/franchises/{franchiseId}/branches/{branchName}/products", handler::addProduct)
+                .PUT("/franchises/{franchiseId}/branches/{branchName}/products/{productName}/stock", handler::updateProductStock)
+                .DELETE("/franchises/{franchiseId}/branches/{branchName}/products/{productName}", handler::deleteProduct)
+                .GET("/franchises/{franchiseId}/branches/products/top-stock", handler::getTopStockProductsByFranchise)
+                .build();
     }
-
-
 }
